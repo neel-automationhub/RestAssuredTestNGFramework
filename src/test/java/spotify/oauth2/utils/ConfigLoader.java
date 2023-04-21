0bd4cca3f7d500a5dc54a494e8d17880de595c99
neel_patel <pneel141@gmail.com>
2023-04-21 12:28:43 +0530

    Configloader message change

--- src/test/java/spotify/oauth2/utils/ConfigLoader.java
+++ src/test/java/spotify/oauth2/utils/ConfigLoader.java
@@ -21,25 +21,25 @@ public class ConfigLoader {
     public String getClientId() {
         String clientId = properties.getProperty("client_id");
         if(clientId != null) return clientId;
-        else throw new RuntimeException("property client_id is not specified in the config.properties file");
+        else throw new RuntimeException("property client_id is not specified in the config.properties file.!");
     }
 
     public String getClientSecret() {
         String clientSecret = properties.getProperty("client_secret");
         if(clientSecret != null) return clientSecret;
-        else throw new RuntimeException("property client_secret is not specified in the config.properties file");
+        else throw new RuntimeException("property client_secret is not specified in the config.properties file.!");
     }
 
     public String getGrantType() {
         String grantType = properties.getProperty("grant_type");
         if(grantType != null) return grantType;
-        else throw new RuntimeException("property grant_type is not specified in the config.properties file");
+        else throw new RuntimeException("property grant_type is not specified in the config.properties file.!");
     }
 
     public String getRefreshToken() {
         String refreshToken = properties.getProperty("refresh_token");
         if(refreshToken != null) return refreshToken;
-        else throw new RuntimeException("property refresh_token is not specified in the config.properties file");
+        else throw new RuntimeException("property refresh_token is not specified in the config.properties file.!");
     }
 
 }
